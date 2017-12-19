4a3dc0d037a3a57e22375269e4d3f1c3da7c1511
bitbaba <imzhhwu@gmail.com>
2017-12-19 10:29:50 +0800

    update cert utilities

--- certutil/1.csr.sh
+++ certutil/1.csr.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-openssl ecparam -genkey -name secp384r1 | openssl ec -out domain.key
+#openssl ecparam -genkey -name secp384r1 | openssl ec -out domain.key
+openssl genrsa 4096 > account.key 
+openssl genrsa 4096 > domain.key
 
-cp /etc/ssl/openssl.cnf ./domain.cnf
+cp -Lf /etc/ssl/openssl.cnf ./domain.cnf
+
 cat <<_heredoc >> ./domain.cnf
 [SAN]
-subjectAltName=DNS:bitbaba.com,DNS:www.bitbaba.com,DNS:bank.bitbaba.com,DNS:pay.bitbaba.com,DNS:pool.bitbaba.com,DNS:bitgold.bitbaba.com,DNS:bitmail.bitbaba.com,DNS:miner.bitbaba.com,DNS:ex.bitbaba.com
 _heredoc
 
-openssl req -new -sha256 -key domain.key -subj "/" -reqexts SAN -config ./domain.cnf > domain.csr
+echo -n "subjectAltName=DNS:bitbaba.com"   >> domain.cnf
+echo -n ",DNS:www.bitbaba.com"             >> domain.cnf
+echo -n ",DNS:bintray.bitbaba.com"         >> domain.cnf
+echo -n ",DNS:brainwallet.bitbaba.com"     >> domain.cnf
+echo -n ",DNS:bank.bitbaba.com"            >> domain.cnf
+echo -n ",DNS:pay.bitbaba.com"             >> domain.cnf
+echo -n ",DNS:pool.bitbaba.com"            >> domain.cnf
+echo -n ",DNS:bitgold.bitbaba.com"         >> domain.cnf
+echo -n ",DNS:bitmail.bitbaba.com"         >> domain.cnf
+echo -n ",DNS:miner.bitbaba.com"           >> domain.cnf
+echo -n ",DNS:ex.bitbaba.com"              >> domain.cnf
+
+openssl req -new -sha256 -key domain.key -subj "/CN=BITBABA.COM" -reqexts SAN -config ./domain.cnf > domain.csr
 
--- certutil/2.req.sh
+++ certutil/2.req.sh
@@ -1,1 +1,10 @@
-acme_tiny.py --account-key ./account.key --csr ./domain.csr --acme-dir /var/www/html/.well-known/acme-challenge/ > ./signed.crt
+#!/bin/bash
+
+# /root/sandbox/var/www/html/.well-known/acme-challenge/
+CHAN_DIR=/root/sandbox/var/www/html/.well-known/acme-challenge/
+
+mkdir -p ${CHAN_DIR}
+
+python acme_tiny.py --account-key ./account.key \
+                    --csr ./domain.csr \
+                    --acme-dir $CHAN_DIR > ./signed.crt
